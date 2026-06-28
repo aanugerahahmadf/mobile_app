0f83d863b411aa4709b419b774bba8a03ff48e2a
aanugerahahmadf <anugerahahmadf16@gmail.com>
2026-06-28 18:03:47 +0700

    fix: pump full 4s in splash test to clear pending timer

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,7 +6,7 @@ import 'package:mobile_app/main.dart';
 void main() {
   testWidgets('App renders splash screen', (WidgetTester tester) async {
     await tester.pumpWidget(const ProviderScope(child: WeddingApp()));
-    await tester.pump(const Duration(seconds: 1));
+    await tester.pump(const Duration(seconds: 4));
     expect(find.byType(MaterialApp), findsOneWidget);
   });
 }
